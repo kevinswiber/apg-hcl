36ceb464f55c3c975630c972788dfa90d431b7be
Kevin Swiber <kswiber@gmail.com>
2016-09-01 16:32:56 -0700

    Adding better validation to ExtractVariables.

--- config/policy/extract_variables.go
+++ config/policy/extract_variables.go
@@ -66,26 +66,26 @@ type evJSONPayload struct {
 
 type evJSONPayloadVariable struct {
 	XMLName  string `xml:"Variable" hcl:"-"`
-	Name     string `xml:"name,attr" hcl:",key"`
+	Name     string `xml:"name,attr" hcl:"-"`
 	Type     string `xml:"type,attr,omitempty" hcl:"type"`
 	JSONPath string `hcl:"json_path"`
 }
 
 type evXMLPayload struct {
-	XMLName               string                  `xml:"XMLPayload" hcl:"-"`
-	StopPayloadProcessing bool                    `xml:"stopPayloadProcessing,attr,omitempty" hcl:"stop_payload_processing"`
-	Namespaces            []*evXMLNamespace       `xml:"Namespaces>Namespace,omitempty" hcl:"namespace"`
-	Variables             []*evXMLPayloadVariable `xml:"Variable" hcl:"variable"`
+	XMLName               string                   `xml:"XMLPayload" hcl:"-"`
+	StopPayloadProcessing bool                     `xml:"stopPayloadProcessing,attr,omitempty" hcl:"stop_payload_processing"`
+	Namespaces            []*evXMLPayloadNamespace `xml:"Namespaces>Namespace,omitempty" hcl:"namespace"`
+	Variables             []*evXMLPayloadVariable  `xml:"Variable" hcl:"variable"`
 }
 
-type evXMLNamespace struct {
-	Prefix string `xml:"prefix,attr,omitempty" hcl:",key"`
+type evXMLPayloadNamespace struct {
+	Prefix string `xml:"prefix,attr,omitempty" hcl:"-"`
 	Value  string `xml:",chardata" hcl:"value"`
 }
 
 type evXMLPayloadVariable struct {
 	XMLName string `xml:"Variable" hcl:"-"`
-	Name    string `xml:"name,attr" hcl:",key"`
+	Name    string `xml:"name,attr" hcl:"-"`
 	Type    string `xml:"type,attr,omitempty" hcl:"type"`
 	XPath   string `hcl:"xpath"`
 }
@@ -143,7 +143,6 @@ func LoadExtractVariablesHCL(item *ast.ObjectItem) (interface{}, error) {
 		if err != nil {
 			errors = multierror.Append(errors, err)
 		} else {
-			fmt.Printf("headers: %+v\n", headers)
 			p.Headers = headers
 		}
 	}
@@ -164,6 +163,37 @@ func LoadExtractVariablesHCL(item *ast.ObjectItem) (interface{}, error) {
 		} else {
 			p.Variables = variables
 		}
+	}
+
+	if jsonPayloadList := listVal.Filter("json_payload"); len(jsonPayloadList.Items) > 0 {
+		jsonPayload, err := loadExtractVariablesJSONPayloadHCL(jsonPayloadList.Items[0])
+		if err != nil {
+			errors = multierror.Append(errors, err)
+		} else {
+			p.JSONPayload = jsonPayload
+		}
+	}
+
+	if xmlPayloadList := listVal.Filter("xml_payload"); len(xmlPayloadList.Items) > 0 {
+		xmlPayload, err := loadExtractVariablesXMLPayloadHCL(xmlPayloadList.Items[0])
+		if err != nil {
+			errors = multierror.Append(errors, err)
+		} else {
+			p.XMLPayload = xmlPayload
+		}
+	}
+
+	if len(p.URIPaths) == 0 && len(p.QueryParams) == 0 &&
+		len(p.FormParams) == 0 && len(p.Variables) == 0 &&
+		p.JSONPayload == nil && p.XMLPayload == nil {
+		pos := item.Val.Pos()
+		newError := hclerror.PosError{
+			Pos: pos,
+			Err: fmt.Errorf("extract variables requires one of uri_path, query_param, " +
+				"header, form_param, json_payload, or xml_payload"),
+		}
+		errors = multierror.Append(errors, &newError)
+
 	}
 
 	if errors != nil {
@@ -287,15 +317,12 @@ func loadExtractVariablesHeadersHCL(items []*ast.ObjectItem) ([]*evHeader, error
 		hdr.Name = item.Keys[0].Token.Value().(string)
 
 		if patternList := listVal.Filter("pattern"); len(patternList.Items) > 0 {
-			fmt.Printf("header pattern count: %d", len(items))
 			patterns, err := loadExtractVariablesPatternsHCL(patternList.Items)
 			if err != nil {
 				return nil, err
 			}
 
 			hdr.Patterns = patterns
-		} else {
-			fmt.Println("No header patterns")
 		}
 
 		headers = append(headers, &hdr)
@@ -393,6 +420,182 @@ func loadExtractVariablesVariablesHCL(items []*ast.ObjectItem) ([]*evVariable, e
 		variables = append(variables, &v)
 	}
 	return variables, nil
+}
+
+func loadExtractVariablesJSONPayloadHCL(item *ast.ObjectItem) (*evJSONPayload, error) {
+	var p evJSONPayload
+
+	var listVal *ast.ObjectList
+	if ot, ok := item.Val.(*ast.ObjectType); ok {
+		listVal = ot.List
+	} else {
+		pos := item.Val.Pos()
+		newError := hclerror.PosError{
+			Pos: pos,
+			Err: fmt.Errorf("json_payload not an object"),
+		}
+		return nil, &newError
+	}
+
+	if err := hcl.DecodeObject(&p, item.Val.(*ast.ObjectType)); err != nil {
+		return nil, err
+	}
+
+	if variableList := listVal.Filter("variable"); len(variableList.Items) > 0 {
+		variables, err := loadExtractVariablesJSONPayloadVariablesHCL(variableList.Items)
+		if err != nil {
+			return nil, err
+		}
+
+		p.Variables = variables
+	}
+
+	return &p, nil
+}
+
+func loadExtractVariablesJSONPayloadVariablesHCL(items []*ast.ObjectItem) ([]*evJSONPayloadVariable, error) {
+	var variables []*evJSONPayloadVariable
+	for _, item := range items {
+		var v evJSONPayloadVariable
+
+		if _, ok := item.Val.(*ast.ObjectType); !ok {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("variable not an object"),
+			}
+			return nil, &newError
+		}
+
+		if err := hcl.DecodeObject(&v, item.Val.(*ast.ObjectType)); err != nil {
+			return nil, err
+		}
+
+		if len(item.Keys) == 0 || item.Keys[0].Token.Value().(string) == "" {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("variable requires a name"),
+			}
+			return nil, &newError
+		}
+
+		v.Name = item.Keys[0].Token.Value().(string)
+
+		variables = append(variables, &v)
+	}
+
+	return variables, nil
+}
+
+func loadExtractVariablesXMLPayloadHCL(item *ast.ObjectItem) (*evXMLPayload, error) {
+	var p evXMLPayload
+
+	var listVal *ast.ObjectList
+	if ot, ok := item.Val.(*ast.ObjectType); ok {
+		listVal = ot.List
+	} else {
+		pos := item.Val.Pos()
+		newError := hclerror.PosError{
+			Pos: pos,
+			Err: fmt.Errorf("xml_payload not an object"),
+		}
+		return nil, &newError
+	}
+
+	if err := hcl.DecodeObject(&p, item.Val.(*ast.ObjectType)); err != nil {
+		return nil, err
+	}
+
+	if namespaceList := listVal.Filter("namespace"); len(namespaceList.Items) > 0 {
+		namespaces, err := loadExtractVariablesXMLPayloadNamespacesHCL(namespaceList.Items)
+		if err != nil {
+			return nil, err
+		}
+
+		p.Namespaces = namespaces
+	}
+
+	if variableList := listVal.Filter("variable"); len(variableList.Items) > 0 {
+		variables, err := loadExtractVariablesXMLPayloadVariablesHCL(variableList.Items)
+		if err != nil {
+			return nil, err
+		}
+
+		p.Variables = variables
+	}
+
+	return &p, nil
+}
+
+func loadExtractVariablesXMLPayloadVariablesHCL(items []*ast.ObjectItem) ([]*evXMLPayloadVariable, error) {
+	var variables []*evXMLPayloadVariable
+	for _, item := range items {
+		var v evXMLPayloadVariable
+
+		if _, ok := item.Val.(*ast.ObjectType); !ok {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("variable not an object"),
+			}
+			return nil, &newError
+		}
+
+		if err := hcl.DecodeObject(&v, item.Val.(*ast.ObjectType)); err != nil {
+			return nil, err
+		}
+
+		if len(item.Keys) == 0 || item.Keys[0].Token.Value().(string) == "" {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("variable requires a name"),
+			}
+			return nil, &newError
+		}
+
+		v.Name = item.Keys[0].Token.Value().(string)
+
+		variables = append(variables, &v)
+	}
+
+	return variables, nil
+}
+
+func loadExtractVariablesXMLPayloadNamespacesHCL(items []*ast.ObjectItem) ([]*evXMLPayloadNamespace, error) {
+	var namespaces []*evXMLPayloadNamespace
+	for _, item := range items {
+		var v evXMLPayloadNamespace
+
+		if _, ok := item.Val.(*ast.ObjectType); !ok {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("namespace not an object"),
+			}
+			return nil, &newError
+		}
+
+		if err := hcl.DecodeObject(&v, item.Val.(*ast.ObjectType)); err != nil {
+			return nil, err
+		}
+
+		if len(item.Keys) == 0 || item.Keys[0].Token.Value().(string) == "" {
+			pos := item.Val.Pos()
+			newError := hclerror.PosError{
+				Pos: pos,
+				Err: fmt.Errorf("namespace requires a name"),
+			}
+			return nil, &newError
+		}
+
+		v.Prefix = item.Keys[0].Token.Value().(string)
+
+		namespaces = append(namespaces, &v)
+	}
+
+	return namespaces, nil
 }
 
 func loadExtractVariablesPatternsHCL(items []*ast.ObjectItem) ([]*evPattern, error) {
